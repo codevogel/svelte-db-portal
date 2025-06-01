b5a9fecde5363249d1d9b2a57229669c58ef8caa
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-01 19:14:17 +0200

    app: add dashboard/session and dashboard/page

---
+++ app/src/routes/dashboard/session/+page.svelte
@@ -0,0 +1,4 @@
+<!-- /src/routes/dashboard/session/+page.svelte -->
+
+<h2 class="text-xl font-bold">Session Page</h2>
+<p>This is the session page.</p>
---
+++ app/src/routes/dashboard/user/+page.svelte
@@ -0,0 +1,4 @@
+<!-- /src/routes/dashboard/user/+page.svelte -->
+
+<h2 class="text-xl font-bold">User Page</h2>
+<p>This is the user page.</p>
