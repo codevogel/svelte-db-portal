c2b227fc7e9ae1698bd8fceb7d575732ae6af0ae
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-01 19:14:36 +0200

    app: use navigation store in root layout

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
+<!-- src/routes/+layout.svelte -->
+
 <script lang="ts">
 	import '../app.css';
 	import NavBar from '$lib/nav/NavBar.svelte';
+	import { pages } from '$lib/stores/navigation';
 
 	let { children } = $props();
 </script>
 
-<NavBar />
+<NavBar pages={$pages} />
 {@render children()}
