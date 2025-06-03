6dec89c943e1ae4507ab31afd185ed5defeba1d3
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-03 10:23:46 +0200

    app: add Theme store

---
+++ app/src/lib/stores/theme.ts
@@ -0,0 +1,11 @@
+import type { Theme } from '$lib/types/theme';
+import { readable, type Readable } from 'svelte/store';
+
+export const themes: Readable<Theme[]> = readable([
+	{ label: '🐱 Catppuccin', value: 'catppuccin' },
+	{ label: '🐺 Cerberus', value: 'cerberus' },
+	{ label: '📺 Vintage', value: 'vintage' },
+	{ label: '💮 Modern', value: 'modern' }
+]);
+
+
