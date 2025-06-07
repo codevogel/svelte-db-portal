f5d26b6f1bf1936ca303ebaa600b4e4f00b0338a
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-07 17:31:18 +0200

    app: add userProfiles table

--- app/src/lib/server/db/schema.ts
+++ app/src/lib/server/db/schema.ts
@@ -1,7 +1,9 @@
 // /src/lib/db/schema.ts
 
 import { mysqlTable, date, datetime, varchar, int } from 'drizzle-orm/mysql-core';
+import { relations } from 'drizzle-orm';
 
+// Holds user information.
 export const users = mysqlTable('users', {
 	id: int('id').primaryKey().autoincrement(),
 	createdAt: datetime('created_at').notNull(),
@@ -9,3 +11,24 @@ export const users = mysqlTable('users', {
 	dateOfBirth: date('date_of_birth').notNull()
 });
 
+// Holds user profile information.
+export const userProfiles = mysqlTable('user_profiles', {
+	id: int('id').primaryKey().autoincrement(),
+	userId: int('user_id').references(() => users.id),
+	firstName: varchar('first_name', { length: 30 }).notNull(),
+	lastName: varchar('last_name', { length: 30 }).notNull(),
+	title: varchar('title', { length: 255 }).notNull()
+});
+
+// One user has one profile.
+export const usersRelations = relations(users, ({ one }) => ({
+	profile: one(userProfiles)
+}));
+
+// One profile belongs to one user.
+export const userProfilesRelations = relations(userProfiles, ({ one }) => ({
+	user: one(users, {
+		fields: [userProfiles.userId],
+		references: [users.id]
+	})
+}));
