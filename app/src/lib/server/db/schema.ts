a6e7f335dbb7f034d329162772303b21106c4e08
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-07 15:17:58 +0200

    app: add users table

--- app/src/lib/server/db/schema.ts
+++ app/src/lib/server/db/schema.ts
@@ -1,6 +1,11 @@
-import { mysqlTable, serial, int } from 'drizzle-orm/mysql-core';
+// /src/lib/db/schema.ts
 
-export const user = mysqlTable('user', {
-	id: serial('id').primaryKey(),
-	age: int('age')
+import { mysqlTable, date, datetime, varchar, int } from 'drizzle-orm/mysql-core';
+
+export const users = mysqlTable('users', {
+	id: int('id').primaryKey().autoincrement(),
+	createdAt: datetime('created_at').notNull(),
+	username: varchar('username', { length: 20 }).unique().notNull(),
+	dateOfBirth: date('date_of_birth').notNull()
 });
+
