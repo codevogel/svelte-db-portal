225fc7a48bdc381e3595e977f25f56b80ed722bb
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-09 14:46:27 +0200

    app: add UserDAO

---
+++ app/src/lib/server/dao/UserDAO.ts
@@ -0,0 +1,16 @@
+import { DAO } from '$lib/server/dao/DAO';
+import type { User } from '$lib/server/db/schema';
+import { users } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
+
+export class UserDAO extends DAO {
+	static async getAllUsers(): Promise<User[]> {
+		return DAO.db.select().from(users);
+	}
+
+	static async findUserById(id: number): Promise<User | undefined> {
+		return DAO.db.query.users.findFirst({
+			where: eq(users.id, id)
+		});
+	}
+}
