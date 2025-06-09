bf085eebb0adb1ec1ef17083686585ef4bd613d0
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-09 14:47:05 +0200

    app: add error handling

---
+++ app/src/lib/ui/views/Error.svelte
@@ -0,0 +1,10 @@
+<!-- /src/lib/ui/views/Error.svelte -->
+
+<script lang="ts">
+	let { status, message }: { status: number, message: string } = $props();
+</script>
+
+<div class="text-center p-8">
+	<h1 class="text-3xl font-bold">Error {status}</h1>
+	<p>{message}</p>
+</div>
---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/state';
+	import Error from '$lib/ui/views/Error.svelte';
+</script>
+
+{#if page.error}
+	<Error status={page.status} message={page.error.message}/>
+{/if}
