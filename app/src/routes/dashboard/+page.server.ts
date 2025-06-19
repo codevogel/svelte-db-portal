6f0c60895e704298a73c8451fa45b4efcb49d3c6
Kamiel de Visser <kamieldevisser@gmail.com>
2025-06-19 15:27:35 +0200

    app: retouch dashboard overview

--- app/src/routes/dashboard/+page.server.ts
+++ app/src/routes/dashboard/+page.server.ts
@@ -3,12 +3,16 @@
 import type { PageServerLoad } from './$types';
 
 import { ScoreDAO, type TopScorer } from '$lib/server/dao/ScoreDAO';
+import { UserDAO } from '$lib/server/dao/UserDAO';
+import type { User } from '$lib/server/db/schema';
 
 export const load: PageServerLoad = async () => {
 	const limit: number = 10;
+	const users: User[] = await UserDAO.getAllUsers();
 	const topScorers: TopScorer[] = await ScoreDAO.getTopScorers(limit);
 
 	return {
+		users,
 		topScorers
 	};
 };
--- app/src/routes/dashboard/+page.svelte
+++ app/src/routes/dashboard/+page.svelte
@@ -2,21 +2,72 @@
 
 <script lang="ts">
 	import type { TopScorer } from '$lib/server/dao/ScoreDAO';
-	
-	let { data } = $props();
+	import type { User } from '$lib/server/db/schema';
+	import Card from '$lib/ui/views/Card.svelte';
+	import type { TableData } from '$lib/ui/views/Table.svelte';
+	import Table from '$lib/ui/views/Table.svelte';
+	import { ageFromDateOfBirth } from '$lib/utils/date.js';
 
-	let topScorers: TopScorer[] = $derived(data.topScorers);
+	const { data } = $props();
+
+	const topScoreTable: TableData = $derived({
+		caption:
+			'A list of the top 10 users who have the highest scores.\nClick to view the session in which they achieved it.',
+		columns: [
+			'Ranking',
+			'Username',
+			'Level ID',
+			'Score',
+			'Accuracy',
+			'Time Taken',
+			'Created At',
+			'Session ID'
+		],
+		rows: data.topScorers.map((score: TopScorer, index: number) => ({
+			values: [
+				index + 1,
+				score.user.username,
+				score.score.levelId,
+				score.score.score,
+				score.score.accuracy,
+				score.score.timeTaken,
+				score.score.createdAt.toLocaleString(),
+				score.session.id
+			],
+			url: `/dashboard/session/${score.session.id}`
+		})),
+		paginationOptions: { enabled: false }
+	});
+
+	const userTable: TableData = $derived({
+		caption: 'A list of users.\nClick to view their profile.',
+		columns: ['Username', 'Age', 'Created At'],
+		rows: data.users.map((user: User) => ({
+			values: [
+				user.username,
+				ageFromDateOfBirth(user.dateOfBirth),
+				user.createdAt.toLocaleDateString()
+			],
+			url: `/dashboard/user/${user.id}`
+		}))
+	});
 </script>
 
-<h2 class="text-xl font-bold">Dashboard home.</h2>
-
-	<div class="grid grid-cols-3 ">
-		<span class="text-lg">Top Scorers</span>
-		<span class="text-lg">Score</span>
-		<span class="text-lg">Session ID</span>
-{#each topScorers as scorer (scorer.user.id)}
-		<span class="text-lg">{scorer.user.username}</span>
-		<span class="text-lg">{scorer.score.score}</span>
-		<span class="text-lg">{scorer.session.id}</span>
-{/each}
-	</div>
+<div class="mx-auto grid grid-cols-1 gap-y-4">
+	<Card baseExtension="!max-w-full w-6xl">
+		{#snippet header()}
+			<h1>Top 10 Users</h1>
+		{/snippet}
+		{#snippet article()}
+			<Table table={topScoreTable} />
+		{/snippet}
+	</Card>
+	<Card baseExtension="!max-w-full w-6xl">
+		{#snippet header()}
+			<h1>Users</h1>
+		{/snippet}
+		{#snippet article()}
+			<Table table={userTable} />
+		{/snippet}
+	</Card>
+</div>
